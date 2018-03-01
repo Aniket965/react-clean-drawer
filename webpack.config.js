99ba1d36f9095a9ac97de98899e9de3aa6e1da50
Aniket965 <Aniket965.as@gmail.com>
2018-03-01 22:36:36 +0530

    wip

---
+++ webpack.config.js
@@ -0,0 +1,110 @@
+
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+const PATH = {
+  src: path.join(__dirname, './src'),
+  lib: path.join(__dirname, './lib'),
+  dist: path.join(__dirname, './dist'),
+  example: path.join(__dirname, './example'),
+  root: path.join(__dirname, './')
+};
+const css = 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]';
+const sass = `${css}!sass`;
+const extractCSS = new ExtractTextPlugin({filename: 'react-clean-drawer.css', allChunks: true});
+const uglify = new webpack.optimize.UglifyJsPlugin({
+  sourceMap: true,
+  minimize: true,
+  compress: {
+    warnings: false
+  }
+});
+
+const CONFIG = {
+  entry: path.join(PATH.src, 'index.js'),
+  externals: {
+    'react': 'React'
+  },
+  devServer: {
+    contentBase: PATH.root,
+    inline: true,
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'es2015']
+          }
+        }
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: {
+          loader: 'file-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: extractCSS.extract({
+          fallback: "style-loader",
+          use: {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: css
+            }
+          }
+        })
+      },
+      {
+        test: /\.scss$/,
+        use: extractCSS.extract({
+          fallback: "style-loader",
+          use: [{
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: sass
+            }
+          },{
+            loader: 'sass-loader'
+          }]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new CaseSensitivePathsPlugin(),
+    extractCSS
+  ]
+};
+
+const umd = Object.assign({}, CONFIG, {
+  output: {
+    libraryTarget: 'umd',
+    library: 'ReactCleanDrawer',
+    filename: 'react-clean-drawer.js',
+    path: PATH.lib
+  }
+});
+
+const umdMinified = Object.assign({}, umd, {
+  output: {
+    libraryTarget: 'umd',
+    library: 'ReactCleanDrawer',
+    filename: 'react-clean-drawer.min.js',
+    path: PATH.lib
+  },
+  plugins: [
+    new CaseSensitivePathsPlugin(),
+    extractCSS,
+    uglify
+  ]
+});
+
+module.exports = [umd, umdMinified];
